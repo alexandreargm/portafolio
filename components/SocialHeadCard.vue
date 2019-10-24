86cfc66f4ec85789bc296249d7a569e8fd23626c
alexandreargm <alexandreargm@gmail.com>
2019-10-24 22:49:39 +0200

    Update SocialHeadCard.vue

--- components/SocialHeadCard.vue
+++ components/SocialHeadCard.vue
@@ -6,14 +6,14 @@
 export default {
   head: {
     meta: [
-      { property: 'og:title', content: 'Página personal de Alexandre Argibay' },
-      { property: 'og:description', content: 'Soy un desarrollador web con estancia en Torrevieja, Alicante especializado en la creación de aplicaciones web intuitivas con personalidad e integrados en back-ends modernos y eficientes.' },
-      { property: 'og:site_name', content: 'Alexandre Argibay | Desarrollador Web' },
-      { property: 'og:image', content: process.env.baseUrl + '/page_preview.png' },
-      { property: 'og:url', content: process.env.baseUrl },
-      { name: 'twitter:image:alt', content: 'Page preview image' },
-      { name: 'twitter:site', content: '@AlexARGM' },
-      { name: 'twitter:card', content: 'summary_large_image' }
+      { property: "og:title", content: "Página personal de Alexandre Argibay" },
+      { property: "og:description", content: "Soy un desarrollador web con estancia en Torrevieja, Alicante especializado en la creación de aplicaciones web intuitivas con personalidad e integrados en back-ends modernos y eficientes." },
+      { property: "og:site_name", content: "Alexandre Argibay | Desarrollador Web" },
+      { property: "og:image", content: process.env.baseUrl + "/page_preview.png" },
+      { property: "og:url", content: process.env.baseUrl },
+      { name: "twitter:image:alt", content: "Page preview image" },
+      { name: "twitter:site", content: "@AlexARGM" },
+      { name: "twitter:card", content: "summary_large_image" }
     ]
   }
 }
