44ddedbd2d3710137d6fdb284f509a6a2c572599
alexandreargm <alexandreargm@gmail.com>
2019-10-24 19:44:37 +0200

    added webmanifest and favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Alexandre Argibay es un desarrollador web con estancia en Torrevieja, Alicante especializado en la creación de aplicaciones web intuitivas con personalidad e integrados en back-ends modernos y eficientes.' },
-      { name: 'theme-color', content: '#00186E' },
+      { name: 'msapplication-TileColor', content: '#00186e' },
+      { name: 'theme-color', content: '#00186e' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#00186e' }
     ]
   },
   /*
