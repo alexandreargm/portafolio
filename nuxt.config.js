7a38d93b2c4f6be6f04484bb04aa5ecb88179199
alexandreargm <alexandreargm@gmail.com>
2019-11-25 15:59:50 +0100

    changed cache settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   render: {
     static: {
       setHeaders (res) {
-        res.setHeader('Cache-control', 'no-cache')
+        res.setHeader('Cache-control', 'max-age=2678400')
       }
     }
   },
@@ -37,7 +37,7 @@ export default {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#0ff' },
+  loading: { color: '#ff0' },
   /*
   ** Global CSS
   */
@@ -73,8 +73,6 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/style-resources-module
     '@nuxtjs/style-resources',
   ],
@@ -96,38 +94,6 @@ export default {
         'postcss-css-variables': { preserve: 'computed' },
         'cssnano': { preset: 'default' } // disabled in dev mode
       }
-    }
-  },
-  cache: {
-    // if you're serving multiple host names (with differing
-    // results) from the same server, set this option to true.
-    // (cache keys will be prefixed by your host name)
-    // if your server is behind a reverse-proxy, please use
-    // express or whatever else that uses 'X-Forwarded-Host'
-    // header field to provide req.hostname (actual host name)
-    useHostPrefix: false,
-    pages: [
-      // these are prefixes of pages that need to be cached
-      // if you want to cache all pages, just include '/'
-      '/',
-    ],
-    
-    key(route, context) {
-      // custom function to return cache key, when used previous
-      // properties (useHostPrefix, pages) are ignored. return 
-      // falsy value to bypass the cache
-    },
-
-    store: {
-      type: 'memory',
-
-      // maximum number of pages to store in memory
-      // if limit is reached, least recently used page
-      // is removed.
-      max: 100,
-
-      // number of seconds to store this page in cache
-      ttl: 60,
     }
   }
 }
