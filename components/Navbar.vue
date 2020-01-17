3b8fb29ed670b457a48be1b6763c9d06012ffc34
alexandreargm <alexandreargm@gmail.com>
2020-01-17 18:42:23 +0100

    added button labels for better accesibility

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -44,6 +44,7 @@
             v-if="showMobileMenu"
             @click="toggleMobileMenu"
             class="menu-toggle__button"
+            aria-label="open menu"
           >
             <svg class="menu-toggle__svg" viewBox="0 0 24 24">
               <path class="menu-toggle__path" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
@@ -54,6 +55,7 @@
             v-else
             @click="toggleMobileMenu"
             class="menu-toggle__button"
+            aria-label="close menu"
           >
             <svg class="menu-toggle__svg" viewBox="0 0 24 24">
               <path class="menu-toggle__path" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
